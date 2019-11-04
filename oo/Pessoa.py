8b50e487213249e382d2f689fb7eaab8c8607189
NatheGobbi <ngobbi1@gmail.com>
2019-11-04 10:53:25 -0300

    criado metodo cumprimentar

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def cumprimentar (self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+#quando se trab com metodos o "p" é visto como primeiro parametro!#
+
+
