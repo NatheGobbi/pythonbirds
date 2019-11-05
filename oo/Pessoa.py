7baa03dda0a502211c6692151054307c77ce022b
NatheGobbi <ngobbi1@gmail.com>
2019-11-05 08:37:33 -0300

    criado e removido atributo dinâmico

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -13,6 +13,11 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filho in luciano.filhos:
         print(filho.nome)
+    luciano.sobrenome = 'Ramaraio'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+
 
 
 #quando se trab com metodos o "p" é visto como primeiro parametro!#
